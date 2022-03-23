9fc14764fe072a4d18011206ff60c20dc831815e
feltfan <emily.bozek@gmail.com>
2022-03-23 16:23:22 -0700

    added comments to server and cleaned up code

--- express_server.js
+++ express_server.js
@@ -1,24 +1,35 @@
 const express = require("express");
-const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+const app = express();
+
+app.set("view engine", "ejs");
+
 const urlDatabase = {
 
 }
+//
+// MIDDLEWARE
+//
 
 app.use(bodyParser.urlencoded({extended: true}), cookieParser());
-app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({extended: true}));
 
+//
+// ROUTES
+//
 
+// no home page
+
+// urls page
 app.get("/urls", (req, res) => {
   const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// create new URL page
 app.get("/urls/new", (req, res) => {
   const templateVars = { username: req.cookies["username"] };
   res.render("urls_new", templateVars);
@@ -30,16 +41,20 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${short}`)
 });
 
+// login in header
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
-});
+  let username = req.body.username;
+  res.cookie("username", username);
+  res.redirect(`/urls`);
+})
 
+// logout in header
 app.post("/logout", (req, res) => {
   res.clearCookie("username", req.body.username);
   res.redirect("/urls");
 });
 
+// shortened URL page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
@@ -62,21 +77,7 @@ app.post("/urls/:id", (req, res) => {
   res.redirect(`/urls/${short}`)
 })
 
-app.post("/login", (req, res) => {
-  let username = req.body.username;
-  res.cookie(username);
-  res.redirect(`/urls`);
-})
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("username", req.body.username);
-  res.redirect("/urls");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
+// tinyURL generator function
 function generateRandomString () {
   let a = "abcdefghijklmnopqrsztuvwyz0123456789";
   let result = "";
@@ -87,3 +88,7 @@ function generateRandomString () {
 
   return result;
 }
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
