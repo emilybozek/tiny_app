789fb9c6f00343208b2191552c1a158e09128865
feltfan <emily.bozek@gmail.com>
2022-03-25 01:43:55 -0700

    fixed syntax so now it's functional

--- express_server.js
+++ express_server.js
@@ -1,9 +1,9 @@
 // Requirements 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
-const { generateRandomString, urlsForUser, getUserByEmail } = require("./helper-functions.js");
+const { generateRandomString, urlsForUser, getUserByEmail } = require('./helper-functions.js');
 
 const PORT = 8080;
 const app = express();
@@ -21,7 +21,10 @@ app.use(cookieSession({
 const urlDatabase = {};
 const users = {};
 
+//
 // ROUTES
+//
+
 // Homepage
 app.get("/", (req, res) => {
   const user = users[req.session.userID];
@@ -31,11 +34,10 @@ app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
-// Mainpage 
+// User's URL database page
 app.get("/urls", (req, res) => {
-  const user = users[req.session.userID];
   if (!req.session.user_id) {
-    return res.send("Not logged in. Please sign in <a href='/login'>Here!</a>");
+    return res.send("Not logged in!");
   }
   const user_id = req.session.user_id;
   const urls = urlsForUser(user_id.id, urlDatabase);
@@ -44,7 +46,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// New URL page 
+// Create tiny URL page 
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
     return res.redirect("/login");
@@ -77,16 +79,16 @@ app.get("/login", (req, res) => {
   res.render("login", templateVars);
 })
 
-// Short URL page and edit page
+// Tiny URL page with edit function
 app.get("/urls/:shortURL", (req, res) => {
   if (!req.session.user_id) {
-    return res.send("Not logged in. Please sign in <a href='/login'>Here!</a>");
+    return res.send("Not logged in!");
   }
   if (!urlDatabase[req.params.shortURL]) {
-    return res.send("Error: Short URL does not exist. <a href='/urls'>Please try again!</a>").status(400);
+    return res.send("Short URL does not exist!").status(400);
   }
   if (req.session.user_id.id !== urlDatabase[req.params.shortURL].userID) {
-    return res.send("Error: You do not own that URL. <a href='/urls'>Please try again!</a>").status(400);
+    return res.send("You do not own that URL!").status(400);
   }
   const user_id = req.session.user_id;
   const shortURL = req.params.shortURL;
@@ -96,7 +98,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// Short URL redirect to long url endpoint
+// Short URL redirect to long url
 app.get("/u/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
     return res.send("Short URL does not exist!");
@@ -111,11 +113,10 @@ app.get("*", (req, res) => {
   res.render("/404");
 });
 
-
-// When short URL is created, add to database
+// Tiny handler
 app.post("/urls", (req, res) => {
   if (!req.session.user_id) {
-    return res.send("Error: Need to be valid user").status(400);
+    return res.send("User not valid!").status(400);
   }
   const shortURL = generateRandomString(6);
   const longURL = req.body.longURL;
@@ -125,17 +126,17 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-// registration handler
+// Registration handler
 app.post("/register", (req, res) => {
   const id = generateRandomString(6);
   const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password); 
 
   if (!email || !req.body.password) {
-  return res.send("Error: Input fields cannot be left blank. <a href='/register'>Please try again!</a>").status(400);
+  return res.send("Error: Input fields cannot be left blank!").status(400);
   }
   if (getUserByEmail(email, users)) {
-    return res.send("Error: User with that email already exists. <a href='/register'>Please try again!</a>").status(400);
+    return res.send("Error: User with that email already exists!").status(400);
   }
 
   users[id] = { id, email, password };
@@ -151,7 +152,7 @@ app.post("/login", (req, res) => {
   if (!getUserByEmail(email, users) ||
     !bcrypt.compareSync(password, getUserByEmail(email, users).password) ||
     getUserByEmail(email, users).email !== email) {
-    return res.send("Error: Invalid credentials. <a href='/login'>Please try again!</a>").status(403);
+    return res.send("Error: Invalid login!").status(403);
   }
   req.session.user_id = users[getUserByEmail(email, users).id];
   res.redirect("/urls");
@@ -166,35 +167,29 @@ app.post("/logout", (req, res) => {
 // Delete handler
 app.post("/urls/:shortURL/delete", (req, res) => {
   if (!req.session.user_id) {
-    return res.send("Error: Not valid user");
+    return res.send("Not valid user!");
   }
   if (req.session.user_id.id !== urlDatabase[req.params.shortURL].userID) {
-    return res.send("Error: You do not own that URL. ").status(400);
+    return res.send("You do not own that URL!").status(400);
   }
   delete urlDatabase[req.params.shortURL];
 
   res.redirect("/urls");
 })
 
-
 // Edit handler
 app.post("/urls/:shortURL", (req, res) => {
   if (!req.session.user_id) {
-    return res.send("Error: Not valid user").status(400);
+    return res.send("Not valid user!").status(400);
   }
   if (req.session.user_id.id !== urlDatabase[req.params.shortURL].userID) {
-    return res.send("Error: You do not own that URL. ").status(400);
+    return res.send("You do not own that URL!").status(400);
   }
   const shortURL = req.params.shortURL;
   const newURL = req.body.newURL;
   urlDatabase[shortURL].longURL = newURL;
   
   res.redirect("/urls");
-});
-
-//
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
